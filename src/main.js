89a6c93329f81a5b1484f6194ebb7a3c458bec82
Simon Haines <nimon@hotmail.co.uk>
2024-03-26 19:16:27 +0000

    adds primevue

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+
+app.mount('#app')
